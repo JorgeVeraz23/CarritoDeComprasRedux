254cb2dddeb6db9fbe0c6a6d5233fabbec1a7d85
JorgeVera <JorgeVeraZambrano@outlook.com>
2024-05-25 21:06:03 -0500

    primer commit uwu

---
+++ y/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='container'>
+          <h1>Titulo</h1>
+          <p>Parrafo</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
