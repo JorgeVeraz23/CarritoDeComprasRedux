f18764e00772723136324bb04cb9c5f099b9723a
JorgeVera <JorgeVeraZambrano@outlook.com>
2024-05-25 23:22:24 -0500

    mas

--- y/src/App.jsx
+++ y/src/App.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react'
-import { useSelector } from 'react-redux'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { setUser, unsetUser } from './reducers/user/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
   const {email, fullName} = useSelector(state => state.user);
 
   return (
     <>
       <div className='container'>
-          <h1>Titulo</h1>
-          <p>El email en el store es: {email}</p>
-          <p>El nombre completo en el store es: {fullName}</p>
+          <h1>Bienvenido</h1>
+          <p>{fullName}</p>
+          <p>Su correo electronico en el store es: {email}</p>
+          {/*En el siguiente boton tenemos un buto que cuando damos
+          click ejecutamos un dispatch en el cual cuando damos click
+          disparamos un action CREATOR*/}
+          <button
+            className='btn btn-primary'
+            onClick={() => {
+              dispatch(setUser({
+                email: "jorgeveraug2@gmail.com",
+                fullName: "Jorge Vera",
+                token: "asdadlkamd", 
+              }
+              ))
+            }}
+          >Cambiar Usuario</button>
+          <button
+            className='btn btn-primary'
+            onClick={() => {
+              dispatch(unsetUser())
+            }}
+          >Cerrar Sesion</button>
       </div>
       
     </>
--- y/src/reducers/user/userSlice.js
+++ y/src/reducers/user/userSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    email: "jorgeveraug2@gmail.com",
-    fullName: "Jorge Vera",
+    email: "",
+    fullName: "",
     token: "",
 }
 
